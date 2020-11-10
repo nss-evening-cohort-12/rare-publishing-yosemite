73398811269556c5e008e356bdcc79c24425b628
Jim Browning <jamalebrowning@gmail.com>
2020-11-09 21:37:16 -0600

    Added modal for delete category

--- src/components/categories/CategoryCards.js
+++ src/components/categories/CategoryCards.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './categories.css'
 
 export const CategoryCards = (props) => {
   const { category, deleteCategory } = props
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
 
   const deleteCategoryEvent = (e) => {
     e.preventDefault();
@@ -15,8 +24,18 @@ export const CategoryCards = (props) => {
       <div className="cat-card-body">
         <h5 className="cat-card-title">{category.name}</h5>
         <button className="btn btn-secondary">Update Category</button>
-        <button className="btn btn-danger" onClick={deleteCategoryEvent}>Delete Category</button>
+        <div>
+      <Button color="danger" onClick={toggle}>Delete</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        {/* <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody> */}
+        <button className="btn btn-danger" onClick={deleteCategoryEvent}>Confirm Delete</button>
+      </Modal>
+    </div>
       </div>
+      
     </div>
   );
 };
