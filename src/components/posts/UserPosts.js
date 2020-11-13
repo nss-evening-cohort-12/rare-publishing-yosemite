0439bd21dfd9b97e8e733c73905d8f41b98d9707
Joshua Medlen <joshua246@gmail.com>
2020-11-12 20:45:06 -0600

    starting user posts

---
+++ src/components/posts/UserPosts.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import postsData from '../utils/postsData'
+
+export const UserPosts = props => {
+  const [getPosts, setPosts] = useState([])
+  
+}
