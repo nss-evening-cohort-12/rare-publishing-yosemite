ed76874ec71bbaffccaccdd45c4c02c5d0ad397c
Joshua Medlen <joshua246@gmail.com>
2020-12-14 20:17:48 -0600

    delete post is working and removes the post. single post view is also working now

--- src/components/posts/PostProvider.js
+++ src/components/posts/PostProvider.js
@@ -4,6 +4,7 @@ export const PostContext = React.createContext()
 
 export const PostProvider = props => {
     const [ posts, setPosts ] = useState([])
+    const [ post, setPost ] = useState({})
     const [ categories, setCategories ] = useState([])
 
     const getPosts = () => {
@@ -17,39 +18,40 @@ export const PostProvider = props => {
         }
 
     const createPost = post => {
-    return fetch('http://localhost:8000/posts', {
-        method: 'POST',
-        headers: {
-        "Authorization": `Token ${localStorage.getItem("r_token")}`,
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(post)
-    })
-        .then(response => response.json())
-        .then(getPosts)
+        return fetch('http://localhost:8000/posts', {
+            method: 'POST',
+            headers: {
+            "Authorization": `Token ${localStorage.getItem("r_token")}`,
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(response => response.json())
+            .then(getPosts)
     }
 
     const updatePost = post => {
-    return fetch(`http://localhost:8000/posts/${post.id}`, {
-        method: 'PUT',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(post)
-    })
-        .then(getPosts)
+        return fetch(`http://localhost:8000/posts/${post.id}`, {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(getPosts)
     }
 
     const deletePost = id => {
-    return fetch(`http://localhost:8000/posts/${id}`, {
-        method: 'DELETE',
-    })
-        .then(getPosts)
+        return fetch(`http://localhost:8000/posts/${id}`, {
+            method: 'DELETE',
+        })
+            .then(getPosts)
     }
 
     const getPostById = (id) => {
-    return fetch(`http://localhost:8000/posts/${id}`)
-    .then(res => res.json())
+        return fetch(`http://localhost:8000/posts/${id}`)
+            .then(res => res.json())
+            .then(setPost)
     }
 
     const getCategories = () => {
@@ -65,6 +67,7 @@ export const PostProvider = props => {
     return (
         <PostContext.Provider value={{
             posts,
+            post,
             getPosts,
             createPost,
             updatePost,
